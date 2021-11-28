40fe902c1e1f85a125258528e12d4e6304227c44
JaneStack <mrumunjanet@gmail.com>
2021-11-28 15:12:59 -0800

    Filtering data by completed done.

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -19,12 +19,23 @@ router.post('/tasks', auth, async(req, res) => {
 }) 
 
 
+// GET /task?completed=true
 router.get('/tasks', auth, async(req,res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+    
     try {
-       const tasks = await Task.find({ owner: req.user._id })
-        res.status(200).send(tasks) 
+        await req.user.populate({
+            path: 'tasks',
+            match
+        })
+        res.send(req.user.tasks)
     } catch (e) {
-        res.status(500).send()
+        console.log(e)
+       res.status(500).send(e) 
     }
 })
 
